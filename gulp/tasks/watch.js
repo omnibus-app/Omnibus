47920a7c34f39f7de3853df9e49e836f8c3e113e
Will LaBranche <wlabranche@gmail.com>
2014-08-06 14:10:09 -0700

    updated watch function, updated travis testing

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
+var livereload = require('livereload');
+var paths = require('../paths');
 
 gulp.task('watch', ['setWatch'], function(){
+  var server = livereload();
   gulp
     .watch('../app/**/*.jade', ['jade']);
+  gulp
+    .watch([
+             paths.dest + 'js/*.js',
+             paths.dest + '**/*.html',
+             paths.dest + 'css/*.css'
+           ], function(event){
+            server.changed(event.path);
+           });
 });
